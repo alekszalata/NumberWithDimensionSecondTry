296dee6eed5a702141b6eb56e547c5a7c1281d6e
aleks <alekszalata@gmail.com>
2017-04-19 14:22:12 +0300

    Остались тесты

--- src/Pack/Main.java
+++ src/Pack/Main.java
@@ -4,7 +4,7 @@ package Pack;
 
 public class Main {
     public static void main(String[] args) {
-        NumberWithDimension numberWithDimension = new NumberWithDimension("3 lol / lel");
+        NumberWithDimension numberWithDimension = new NumberWithDimension("3 1 / lel");
         NumberWithDimension numberWithDimension1 = new NumberWithDimension("2 lol / lel");
         System.out.println(numberWithDimension.division(numberWithDimension1));
     }
